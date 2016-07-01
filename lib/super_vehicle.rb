fab2f36068803fc0ba9f752585c488f8a15eb5c3
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-01 17:15:15 -0400

    Done.

--- lib/super_vehicle.rb
+++ lib/super_vehicle.rb
@@ -1,5 +1,17 @@
 class Vehicle
 
- 
+	attr_accessor :wheel_number, :wheel_size
+
+	def initialize (num,size )
+		@wheel_number = num
+		@wheel_size = size
+	end	
     
+	def go
+		"vrrrrrrrooom!"
+	end
+
+	def fill_up_tank
+		"filling up!"
+	end
 end
